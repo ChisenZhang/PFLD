5582765b33eafc6922b5d4fb4321acd7c435273a
张西群 <xiqun.zhang@17zuoye.com>
2019-07-31 19:49:15 +0800

    edit data augu

--- mobileNet/dataLoader/WIDE_FACE/data_augment.py
+++ mobileNet/dataLoader/WIDE_FACE/data_augment.py
@@ -201,7 +201,7 @@ class preproc(object):
 
     def __call__(self, image, targets):
         assert targets.shape[0] > 0, "this image does not have gt"
-
+        minLen = 16
         boxes = targets[:, :-1].copy()
         labels = targets[:, -1].copy()
 
@@ -216,10 +216,10 @@ class preproc(object):
         image_t = _resize_subtract_mean(image_t, self.img_dim, self.rgb_means, self.rgb_norm)
         boxes_t[:, 0::2] /= width
         boxes_t[:, 1::2] /= height
-        # delInds = np.where(np.logical_and(boxes_t[: 2]*256. < 32, boxes_t[: 3]*256. < 32))
-        # boxes_t = np.delete(boxes_t, np.squeeze(delInds))
-        # labels_t = np.delete(labels_t, np.squeeze(delInds))
-        if boxes_t.size < 1:
+        delInds = np.where(np.logical_or(boxes_t[:, 2]*float(self.img_dim) < minLen, boxes_t[:, 3]*float(self.img_dim) < minLen))
+        boxes_t = np.delete(boxes_t, np.squeeze(delInds), axis=0)
+        labels_t = np.delete(labels_t, np.squeeze(delInds), axis=0)
+        if boxes_t.shape[0] < 1:
             return None, None
 
         labels_t = np.expand_dims(labels_t, 1)
